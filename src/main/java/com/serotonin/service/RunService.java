c85084dd7b21f1e801275229a204a7a3b0d2fac5
kongfancheng123 <13584882827@163.com>
2018-11-16 17:42:48 +0800

    提取出rdu和tcp的run方法

---
+++ src/main/java/com/serotonin/service/RunService.java
@@ -0,0 +1,10 @@
+package com.serotonin.service;
+
+/**
+ * Create by fchkong on 2018/11/16.
+ */
+public interface RunService {
+    void tcpRun() throws Exception;
+
+    void rduRun() throws Exception;
+}
